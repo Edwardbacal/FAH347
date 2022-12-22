3993053e1e6cdb2ecd37863af8230dc742e3a61e
Edwardbacal <edwarddbacal@gmail.com>
2022-12-22 10:56:24 +0900

    added essay

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 $('.padB').on("click", function() {
   $(this).next(".displayNone").slideToggle();
 });
+
+$('#ess').on("click", function() {
+  $("#journal").slideUp();
+  $("#essay").slideToggle();
+});
+
+$('#jour').on("click", function() {
+  $("#essay").slideUp();
+
+  $("#journal").slideToggle();
+});
