f2ab2881443cf75439594b3f82e50af2bb2d65e1
Edwardbacal <edwarddbacal@gmail.com>
2022-12-31 16:13:54 +0800

    live for class

--- index.js
+++ index.js
@@ -14,23 +14,11 @@ $('#jour').on("click", function() {
 });
 
 
-$('#menu').on("click", function(e) {
-  e.preventDefault();
-  $('#siteMenu').css("right", "0")
-  $('#closeMenu').fadeIn();
-});
 
-$('#closeMenu').on("click", function() {
-  $('#siteMenu').css("right", "-100vw")
-  $(this).fadeOut();
-});
 
-$(document ).ready(function() {
-  $("body").append(menu);
-});
 
 const menu = `
-  <header id="siteMenu" style="position: fixed; top: 0; right: -100vw; background: #fff; transition: .4s ease-out; overflow-y: scroll; z-index: 99; height: 100vh;">
+  <header id="siteMenu" style="position: fixed; top: 0; right: -150vw; background: #fff; transition: .4s ease-out; overflow-y: scroll; z-index: 99; height: 100vh;">
 
  
     <h2 style="margin-top: 3.5rem;">
@@ -38,21 +26,21 @@ const menu = `
     </h2>
     <ol class="grid2">
     <li class="padB">
-    <a href="#details">
+    <a href="index.html#details">
     <h4>
     Details
     </h4>
     </a>
     </li>
     <li class="padB">
-    <a href="#assignments">
+    <a href="index.html#assignments">
     <h4>
     Assignments
     </h4>
     </a>
     </li>
     <li class="padB">
-    <a href="">
+    <a href="index.html#schedule">
     <h4>
     Schedule
     </h4>
@@ -106,3 +94,26 @@ const menu = `
     </ol>
   </header>
 `
+
+$('#menu').on("click", function(e) {
+  e.preventDefault();
+  $('#siteMenu').css("right", "0")
+  $('#closeMenu').fadeIn();
+});
+
+$('#closeMenu').on("click", function() {
+  $('#siteMenu').css("right", "-150vw")
+  $(this).fadeOut();
+});
+
+
+$(document ).ready(function() {
+  $("body").append(menu);
+});
+
+
+$(".down").click(function() {
+  $('html, body').animate({
+      scrollTop: $(this).offset().top + 780
+  }, 400);
+});
